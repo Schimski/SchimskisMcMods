0d27d56ca7a2bc0dce990f1bb8af4842b1f04f5a
Schimski <amdsimski@gmail.com>
2014-10-16 09:06:16 +0200

    Fixed shift clicking

--- src/main/java/de/schimski/bulbs/inventory/ContainerBulbs.java
+++ src/main/java/de/schimski/bulbs/inventory/ContainerBulbs.java
@@ -2,15 +2,121 @@ package de.schimski.bulbs.inventory;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public abstract class ContainerBulbs extends Container
 {
     protected final int PLAYER_INVENTORY_ROWS = 3;
     protected final int PLAYER_INVENTORY_COLUMNS = 9;
+    
     @Override
     public boolean canInteractWith(EntityPlayer entityPlayer)
     {
         return true;
     }
+
+    //@Override
+    protected boolean mergeItemStack(ItemStack itemStack, int fromSlot, int toSlot, boolean backwards)
+    {
+        boolean flag1 = false;
+        int k = fromSlot;
+
+        if (backwards)
+        {
+            k = toSlot - 1;
+        }
+
+        Slot slot;
+        ItemStack newItemStack;
+
+
+        if (itemStack.isStackable())
+        {
+            while (itemStack.stackSize > 0 && (!backwards && k < toSlot || backwards && k >= fromSlot))
+            {
+                slot = (Slot)this.inventorySlots.get(k);
+                newItemStack = slot.getStack();
+
+                if (newItemStack != null && newItemStack.getItem() == itemStack.getItem() && (!itemStack.getHasSubtypes() || itemStack.getItemDamage() == newItemStack.getItemDamage()) && ItemStack.areItemStackTagsEqual(itemStack, newItemStack))
+                {
+                    int l = newItemStack.stackSize + itemStack.stackSize;
+
+                    if (l <= itemStack.getMaxStackSize())
+                    {
+                        itemStack.stackSize = 0;
+                        newItemStack.stackSize = l;
+                        slot.onSlotChanged();
+                        flag1 = true;
+                    }
+                    else if (newItemStack.stackSize < itemStack.getMaxStackSize())
+                    {
+                        itemStack.stackSize -= itemStack.getMaxStackSize() - newItemStack.stackSize;
+                        newItemStack.stackSize = itemStack.getMaxStackSize();
+                        slot.onSlotChanged();
+                        flag1 = true;
+                    }
+                }
+
+                if (backwards)
+                {
+                    --k;
+                }
+                else
+                {
+                    ++k;
+                }
+            }
+        }
+
+        if (itemStack.stackSize > 0)
+        {
+            if (backwards)
+            {
+                k = toSlot - 1;
+            }
+            else
+            {
+                k = fromSlot;
+            }
+
+            while (!backwards && k < toSlot || backwards && k >= fromSlot)
+            {
+                slot = (Slot)this.inventorySlots.get(k);
+                newItemStack = slot.getStack();
+
+                if (newItemStack == null)
+                {
+                    if (k==0) {
+                        newItemStack = itemStack.copy();
+                        newItemStack.stackSize = 1;
+                        slot.putStack(newItemStack.copy());
+                        slot.onSlotChanged();
+                        itemStack.stackSize = itemStack.stackSize-1;
+                        flag1 = true;
+                        break;
+                    } else {
+                        slot.putStack(itemStack.copy());
+                        slot.onSlotChanged();
+                        itemStack.stackSize = 0;
+                        flag1 = true;
+                        break;
+                    }
+                }
+
+                if (backwards)
+                {
+                    --k;
+                }
+                else
+                {
+                    ++k;
+                }
+            }
+        }
+
+        return flag1;
+    }
+
 
 }
--- src/main/java/de/schimski/bulbs/inventory/ContainerGridLight.java
+++ src/main/java/de/schimski/bulbs/inventory/ContainerGridLight.java
@@ -1,6 +1,7 @@
 package de.schimski.bulbs.inventory;
 
 import de.schimski.bulbs.inventory.slot.SlotBulbs;
+import de.schimski.bulbs.item.ItemBulbs;
 import de.schimski.bulbs.tileEntity.TileEntityGridLight;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -8,7 +9,7 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
-public class ContainerGridLight extends Container {
+public class ContainerGridLight extends ContainerBulbs {
 
     protected TileEntityGridLight tileEntityGridLight;
 
@@ -19,7 +20,7 @@ public class ContainerGridLight extends Container {
         //and the x-y coordinates it resides on-screen
         //for (int i = 0; i < 3; i++) {
         //  for (int j = 0; j < 3; j++) {
-        addSlotToContainer(new SlotBulbs(tileEntityGridLight, 0, 90, 18));
+        addSlotToContainer(new SlotBulbs(tileEntityGridLight, 0, 108, 37));
         //}
         //}
 
@@ -47,7 +48,106 @@ public class ContainerGridLight extends Container {
 
     @Override
     public ItemStack transferStackInSlot(EntityPlayer entityPlayer, int slotIndex) {
+
         ItemStack newItemStack = null;
+        Slot slot = (Slot) inventorySlots.get(slotIndex);
+        SlotBulbs gridLightSlot = (SlotBulbs) inventorySlots.get(0);
+        if (slot != null && slot.getHasStack()) {
+
+            ItemStack itemStack = slot.getStack();
+            newItemStack = itemStack.copy();
+
+
+
+            if (!gridLightSlot.getHasStack() && slotIndex > 0 && itemStack.getItem() instanceof ItemBulbs) {
+
+                // Slot in gridLight is empty
+                // clicked itemStack is a lightBulb
+                // Click from inventory to gridLight
+
+                newItemStack.stackSize = 1;
+                itemStack.stackSize--;
+
+                slot.putStack(itemStack);
+                slot.onSlotChanged();
+
+                gridLightSlot.putStack(newItemStack);
+                gridLightSlot.onSlotChanged();
+
+                // We do not want the rest of the stack to change position in inventory if 1 item has been sent to the gridLight
+
+                if (itemStack.stackSize == 0)
+                {
+
+                    // If clicked stack has been completely send to another slot
+                    // mark clocked slot EMPTY
+
+                    slot.putStack(null);
+                }
+                else
+                {
+
+                    // If clicked stack has NOT been completely send to another slot
+                    // mark clocked slot as CHANGED
+
+                    slot.onSlotChanged();
+                }
+
+                return null;
+
+            } else if (gridLightSlot.getHasStack() && slotIndex == 0) {
+
+                // Slot in gridLight is NOT empty
+                // Click from gridLight to inventory or ActionBar
+
+                if (!this.mergeItemStack(itemStack, 1, inventorySlots.size(), false))
+                {
+                        return null;
+                }
+            } else if (/*gridLightSlot.getHasStack() && */ slotIndex > 0 && (slotIndex < inventorySlots.size()-9)) {
+
+                // Slot in gridLight is NOT empty
+                // Click from inventory to ActionBar
+
+                if (!this.mergeItemStack(itemStack, inventorySlots.size()-9, inventorySlots.size(), false))
+                {
+                    return null;
+                }
+            } else if (/*gridLightSlot.getHasStack() && */ slotIndex >= (inventorySlots.size()-9)) {
+
+                // Slot in gridLight is NOT empty
+                // Click from ActionBar to inventory
+
+                if (!this.mergeItemStack(itemStack, 1, inventorySlots.size() - 9, false)) {
+                    return null;
+                }
+            }
+
+            if (itemStack.stackSize == 0)
+            {
+
+                // If clicked stack has been completely send to another slot
+                // mark clocked slot EMPTY
+
+                slot.putStack(null);
+            }
+            else
+            {
+
+                // If clicked stack has NOT been completely send to another slot
+                // mark clocked slot as CHANGED
+
+                slot.onSlotChanged();
+            }
+        }
+
+        return newItemStack;
+    }
+
+
+
+
+        /*ItemStack newItemStack = null;
         Slot slot = (Slot) inventorySlots.get(slotIndex);
         if (slot != null && slot.getHasStack())
         {
@@ -64,6 +164,10 @@ public class ContainerGridLight extends Container {
             {
                 return null;
             }
+            else if (!this.mergeItemStack(itemStack, 1, inventorySlots.size(), false))
+            {
+                return null;
+            }
             if (itemStack.stackSize == 0)
             {
                 slot.putStack(null);
@@ -74,5 +178,7 @@ public class ContainerGridLight extends Container {
             }
         }
         return newItemStack;
-    }
+    }*/
+
+
 }
