f1a694870f98c1e6877756dc199da697a7adefea
Schimski <amdsimski@gmail.com>
2014-10-23 21:05:36 +0200

    changes to thinLight Renderer

--- src/main/java/de/schimski/bulbs/renderer/RendererThinLight.java
+++ src/main/java/de/schimski/bulbs/renderer/RendererThinLight.java
@@ -183,10 +183,12 @@ public class RendererThinLight extends TileEntitySpecialRenderer {
         GL11.glPopMatrix();
 
         GL11.glPushMatrix();
+            GL11.glDisable(GL11.GL_LIGHTING);
             GL11.glEnable(GL11.GL_BLEND);
             renderModelAlpha(thinLight);
             GL11.glPopMatrix();
             GL11.glDisable(GL11.GL_BLEND);
+            GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glPopMatrix();
     }
 }
